cda466613fcfff6b1493008f2018fbfad4ed3e86
Med_User <mohamedermili5@gmail.com>
2025-09-20 18:36:32 +0100

    ermed.01

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <div className='flex items-center lg:items-start flex-col gap-4  lg:gap-5'>
               {/* <h1 className="font-display text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold"> Ermili<br className="hidden lg:block" /> Mohamed </h1> */}
               <div className="flex flex-col  gap-3">
-                <h1 className=' text-center md:text-start  text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-bold leading-[1.3] relative font-orbitron'>
+                <h1 className=' text-center md:text-start  text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl  font-bold leading-[1.3] relative font-orbitron'>
                   I&apos;m Ermili <br className="hidden lg:block" /> Mohamed
                 </h1>
                 <h2 className='text-center lg:text-start font-semibold text-xl text-display font-display'>
